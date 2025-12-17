2ecc9c20d0de42c09b50b78914c6d0fb65db06b3
Timur Tsokov <tsokov2t1m5@gmail.com>
2025-12-17 23:22:55 +0200

    update v7

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -1,17 +1,20 @@
-import {z} from "zod";
+import { z } from "zod";
 
 export const signInSchema = z.object({
-    email: z.string({ required_error: "Email is required" })
+    email: z.string()
         .min(1, "Email is required")
         .email("Invalid email"),
-    password: z.string({ required_error: "Password is required" })
+
+    password: z.string()
         .min(1, "Password is required")
         .min(6, "Password must be more than 8 characters")
         .max(32, "Password must be less than 32 characters"),
-})
+});
+
 
 export const ingredientSchema = z.object({
     name: z.string().min(1, "Name is required"),
+
     category: z.enum([
         "VEGETABLES",
         "FRUITS",
@@ -20,9 +23,19 @@ export const ingredientSchema = z.object({
         "SPICES",
         "OTHER",
     ]),
-    unit: z.enum(["GRAMS", "KILOGRAMS", "LITERS", "MILLILITERS", "PIECES"]),
-    pricePerUnit: z.number({ invalid_type_error: "Price must be a number" })
+
+    unit: z.enum([
+        "GRAMS",
+        "KILOGRAMS",
+        "LITERS",
+        "MILLILITERS",
+        "PIECES",
+    ]),
+
+    pricePerUnit: z
+        .number()
         .min(0, "The price must be positive")
         .nullable(),
+
     description: z.string().optional(),
 });
